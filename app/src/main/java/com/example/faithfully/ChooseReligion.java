3313d28acb6c8c556069719c47b6f7e660930707
CaileyBeth <71476736+CaileyBeth@users.noreply.github.com>
2020-09-19 11:10:06 -0700

    Spinner added into ChooseReligionActivity

--- app/src/main/java/com/example/faithfully/ChooseReligion.java
+++ app/src/main/java/com/example/faithfully/ChooseReligion.java
@@ -1,14 +1,42 @@
 package com.example.faithfully;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import android.os.Bundle;
 
-public class ChooseReligion extends AppCompatActivity {
+public class ChooseReligion extends AppCompatActivity implements
+    AdapterView.OnItemSelectedListener {
+    String[] religion = { "Christianity", "Judaism", "Islam", "Religiously Unaffiliated", "Buddhism", "Hinduism","Atheism"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_religion);
+        //Getting the instance of Spinner and Applying OnItemSelectedListener on it
+        Spinner spin = (Spinner) findViewById(R.id.religionSpn);
+        spin.setOnItemSelectedListener(this);
+
+        //Creating the ArrayAdapter instance having the country list
+        ArrayAdapter aa = new ArrayAdapter(this,android.R.layout.simple_spinner_item,religion);
+        aa.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        //Setting the ArrayAdapter data on the Spinner
+        spin.setAdapter(aa);
+
+    }
+
+    //Performing action onItemSelected and onNothing selected
+    @Override
+    public void onItemSelected(AdapterView<?> arg0, View arg1, int position, long id) {
+        Toast.makeText(getApplicationContext(),religion[position] , Toast.LENGTH_LONG).show();
+    }
+    @Override
+    public void onNothingSelected(AdapterView<?> arg0) {
+        // TODO Auto-generated method stub
     }
 }
