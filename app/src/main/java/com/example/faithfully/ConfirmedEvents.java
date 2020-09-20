f19b44eb034f3ff20f02b2a04eb424fd9d513c69
ifeadikanwa <ezeifyyy@gmail.com>
2020-09-20 06:19:20 -0400

    still fixing bugs and prepping for demo

--- app/src/main/java/com/example/faithfully/ConfirmedEvents.java
+++ app/src/main/java/com/example/faithfully/ConfirmedEvents.java
@@ -19,6 +19,8 @@ public class ConfirmedEvents extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_confirmed_events);
+
+        setupRecyclerview();
     }
 
     @Override
